660b4f3a31f8a9b24364937d8427cb120418ed07
Dean Lofts <dean@deanlofts.xyz>
2025-04-02 21:53:04 +1100

    changes

--- astro.config.ts
+++ astro.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: process.env.CI
-    ? "https://astro-shadcn-ui-template.vercel.app"
+    ? "https://shadcn.deanlofts.xyz"
     : "http://localhost:4321",
   integrations: [react()],
   vite: {
